670bdddafb73bca8b1131bf7f86903d1a7bd2a2a
inq39 <61730491+inq39@users.noreply.github.com>
2021-02-24 20:08:18 +0100

    bugfix

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -63,17 +63,18 @@ public class EnemyController : MonoBehaviour
             _explosionSound.Play();
             Destroy(GetComponent<Collider2D>());
             Destroy(GetComponent<EnemyController>());
-            _enemySpeed = 0f;
+            _enemySpeed = 0.5f;
             Destroy(this.gameObject, 2.8f);
             Destroy(trigger.gameObject);
         }
 
         if (trigger.gameObject.CompareTag("Player"))
         {
-            _enemySpeed = 0f;
+            _enemySpeed = 0.5f;
             _explosionSound.Play();
             _destroyEnemyAnimator.SetTrigger("IsEnemyDestroyed");
-            
+            Destroy(GetComponent<Collider2D>());
+            Destroy(GetComponent<EnemyController>());
             Destroy(this.gameObject, 2.8f);
            
 
